83267c6d6893bf120fcaa240ed4ba3706f0b8953
Glauber <glauber.bannwart@gmail.com>
2023-08-29 19:16:43 -0300

    update schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,8 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
-);
+    name varchar(100),
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg decimal);
