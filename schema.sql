15f6284c902db7e8d2fc64797b1613533b0e3d04
Glauber <glauber.bannwart@gmail.com>
2023-08-30 18:39:53 -0300

    Add ID Primary Key

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
+    id serial PRIMARY KEY,
     name varchar(100),
     date_of_birth date,
     escape_attempts integer,
