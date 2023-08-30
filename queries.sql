829cb70ae640056e61d1dd651fce54dcc8e4e91f
Glauber <glauber.bannwart@gmail.com>
2023-08-30 19:09:18 -0300

    Update Query List

--- queries.sql
+++ queries.sql
@@ -5,7 +5,7 @@ SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 
 -- List the name of all animals that are neutered and have less than 3 escape attempts.
-SELECT * FROM animals WHERE neutered = true AND escape_attempts < 3;
+SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
 
 -- List the date of birth of all animals named either "Agumon" or "Pikachu".
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
