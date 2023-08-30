f7232c90d173586a5b2678813b4ce5c8be2cc4e1
Glauber <glauber.bannwart@gmail.com>
2023-08-30 16:34:33 -0300

    Update description

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
 
--- Insert the following data:
+-- Task Description> Insert the following data:
 -- Animal: His name is Agumon. He was born on Feb 3rd, 2020, and currently weighs 10.23kg. He was neutered and he has never tried to escape.
 -- Animal: Her name is Gabumon. She was born on Nov 15th, 2018, and currently weighs 8kg. She is neutered and she has tried to escape 2 times.
 -- Animal: His name is Pikachu. He was born on Jan 7th, 2021, and currently weighs 15.04kg. He was not neutered and he has tried to escape once.
