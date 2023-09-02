5b74e263e2db9c322d0e7ea5aaf285310e0f1ce8
Glauber <glauber.bannwart@gmail.com>
2023-09-01 23:25:17 -0300

    Update Data with refactor

--- data.sql
+++ data.sql
@@ -1,60 +1,107 @@
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Agumon', '2020-02-03', 10.23, true, 0);
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Gabumon', '2018-11-15', 8, true, 2);
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Pikachu', '2021-01-07', 15.04, false, 1);
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Devimon', '2017-05-12', 11, true, 5);
+-- Active: 1693619777535@@127.0.0.1@5432@postgres
+insert into animals (name, date_of_birth, escape_attempts, neutered, weight_kg) values 
+('Agumon', '2020-02-03', 0, true, 10.23),
+('Gabumon', '2018-11-15', 2, true, 8.0),
+('Pikachu', '2021-01-07', 1, false, 15.04),
+('Devimon', '2017-05-13', 5, true, 11.0),
+('Charmander', '2020-02-08', 0, false, -11.0),
+('Plantmon', '2021-11-15', 2, true, -5.7),
+('Squirtle', '1993-04-02', 3, false, -12.13),
+('Angemon', '2005-06-12', 1, true, -45.0),
+('Boarmon', '2005-06-7', 7, true, 20.4),
+('Blossom', '1998-10-13', 3, true, 17.0),
+('Ditto', '2022-05-14', 4, true, 22.0);
 
--- add the data to the existing table of animals
--- Animal: His name is Charmander. He was born on Feb 8th, 2020, and currently weighs -11kg. He is not neutered and he has never tried to escape.
--- Animal: Her name is Plantmon. She was born on Nov 15th, 2021, and currently weighs -5.7kg. She is neutered and she has tried to escape 2 times.
--- Animal: His name is Squirtle. He was born on Apr 2nd, 1993, and currently weighs -12.13kg. He was not neutered and he has tried to escape 3 times.
--- Animal: His name is Angemon. He was born on Jun 12th, 2005, and currently weighs -45kg. He is neutered and he has tried to escape once.
--- Animal: His name is Boarmon. He was born on Jun 7th, 2005, and currently weighs 20.4kg. He is neutered and he has tried to escape 7 times.
--- Animal: Her name is Blossom. She was born on Oct 13th, 1998, and currently weighs 17kg. She is neutered and she has tried to escape 3 times.
--- Animal: His name is Ditto. He was born on May 14th, 2022, and currently weighs 22kg. He is neutered and he has tried to escape 4 times.
+insert into owners (name, age) values 
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
 
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Charmander', '2020-02-08', -11, false, 0);
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Plantmon', '2021-11-15', -5.7, true, 2);
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Squirtle', '1993-04-02', -12.13, false, 3);
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Angemon', '2005-06-12', -45, true, 1);
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Boarmon', '2005-06-07', 20.4, true, 7);
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Blossom', '1998-10-13', 17, true, 3);
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Ditto', '2022-05-14', 22, true, 4);
+insert into species (name) values 
+('Digimon'),
+('Pokemon');
 
--- Insert the following data into the owners table:
--- Sam Smith 34 years old.
--- Jennifer Orwell 19 years old.
--- Bob 45 years old.
--- Melody Pond 77 years old.
--- Dean Winchester 14 years old.
--- Jodie Whittaker 38 years old.
--- Insert the following data into the species table:
--- Pokemon
--- Digimon
--- Modify your inserted animals so it includes the species_id value:
--- If the name ends in "mon" it will be Digimon
--- All other animals are Pokemon
--- Modify your inserted animals to include owner information (owner_id):
--- Sam Smith owns Agumon.
--- Jennifer Orwell owns Gabumon and Pikachu.
--- Bob owns Devimon and Plantmon.
--- Melody Pond owns Charmander, Squirtle, and Blossom.
--- Dean Winchester owns Angemon and Boarmon.
+update animals set species_id = (
+    CASE 
+        when name like '%mon' then (select id from species where name = 'Digimon')
+        else (select id from species where name = 'Pokemon')
+    END
+);
 
-INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
-INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
-INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
-INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
-INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
-INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+update animals set owner_id = (select id from owners where name = 'Sam Smith')
+where name = 'Agumon';
 
-INSERT INTO species (name) VALUES ('Pokemon');
-INSERT INTO species (name) VALUES ('Digimon');
+update animals set owner_id = (select id from owners where name = 'Jennifer Orwell') 
+where name in ('Gabumon', 'Pikachu');
 
-UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
-UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
+update animals set owner_id = (select id from owners where name = 'Bob')
+where name in ('Devimon', 'Plantmon');
 
-UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
-UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon' OR name = 'Pikachu';
-UPDATE animals SET owner_id = 3 WHERE name = 'Devimon' OR name = 'Plantmon';
-UPDATE animals SET owner_id = 4 WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
-UPDATE animals SET owner_id = 5 WHERE name = 'Angemon' OR name = 'Boarmon';
+update animals set owner_id = (select id from owners where name = 'Melody Pond')
+where name in ('Charmander', 'Squirtle', 'Blossom');
+
+update animals set owner_id = (select id from owners where name = 'Dean Winchester')
+where name in ('Angemon', 'Boarmon');
+
+insert into vets (name, age, date_of_graduation) values 
+('William Tatcher', 45, '2000-04-23'),
+('Maisy Smith', 26, '2019-01-17'),
+('Stephanie Mendez', 64, '1981-05-04'),
+('Jack Harkness', 38, '2008-06-08');
+
+insert into specializations(vet_id, species_id) values 
+(1, 1), 
+(3, 1), 
+(3, 2), 
+(4, 2); 
+
+--Vet William Tatcher is specialized in Pokemon.
+--Vet Stephanie Mendez is specialized in Digimon and Pokemon.
+
+--Vet Jack Harkness is specialized in Digimon.
+
+insert into visits values
+(1, 1, '2020-05-24'), 
+(1, 3, '2020-07-22'), 
+(2, 4, '2021-02-02'), 
+(3, 2, '2020-01-05'),
+(3, 2, '2020-03-08'), 
+(3, 2, '2020-05-14'), 
+(4, 2, '2021-05-04'), 
+(5, 1, '2021-02-24'), 
+(6, 2, '2019-12-21'), 
+(6, 1, '2020-08-10'), 
+(6, 2, '2021-04-07'), 
+(7, 3, '2019-09-29'), 
+(8, 4, '2020-10-03'), 
+(8, 4, '2020-11-04'), 
+(9, 2, '2019-01-24'), 
+(9, 2, '2019-05-15'), 
+(9, 2, '2020-02-27'), 
+(9, 2, '2020-08-03'), 
+(10, 3, '2020-05-24'), 
+(10, 1, '2021-01-11'); 
+
+-- Agumon visited William Tatcher on May 24th, 2020.
+-- Agumon visited Stephanie Mendez on Jul 22th, 2020.
+-- Gabumon visited Jack Harkness on Feb 2nd, 2021.
+-- Pikachu visited Maisy Smith on Jan 5th, 2020.
+-- Pikachu visited Maisy Smith on Mar 8th, 2020.
+-- Pikachu visited Maisy Smith on May 14th, 2020.
+-- Devimon visited Stephanie Mendez on May 4th, 2021.
+-- Charmander visited Jack Harkness on Feb 24th, 2021.
+-- Plantmon visited Maisy Smith on Dec 21st, 2019.
+-- Plantmon visited William Tatcher on Aug 10th, 2020.
+-- Plantmon visited Maisy Smith on Apr 7th, 2021.
+-- Squirtle visited Stephanie Mendez on Sep 29th, 2019.
+-- Angemon visited Jack Harkness on Oct 3rd, 2020.
+-- Angemon visited Jack Harkness on Nov 4th, 2020.
+-- Boarmon visited Maisy Smith on Jan 24th, 2019.
+-- Boarmon visited Maisy Smith on May 15th, 2019.
+-- Boarmon visited Maisy Smith on Feb 27th, 2020.
+-- Boarmon visited Maisy Smith on Aug 3rd, 2020.
+-- Blossom visited Stephanie Mendez on May 24th, 2020.
+-- Blossom visited William Tatcher on Jan 11th, 2021.
